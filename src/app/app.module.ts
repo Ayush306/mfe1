0df2c92e04f163fa6b310e69439da18e5fada3ca
ayush.tomar <ayush.tomar@worldfashionexchange.com>
2024-12-27 17:02:56 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { PopupComponent } from './popup/popup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // PopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    FormsModule,
+    MatDialogModule
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
